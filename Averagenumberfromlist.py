35f14f37a3f38f6ec9e46c1003c3e1e6f010b704
Ajinkyasarmalkar <sarmalkaraju@gmial.com>
2020-06-29 22:11:47 +0530

    practice 29

--- Averagenumberfromlist.py
+++
@@ -1,7 +0,0 @@
-n = int(input("enter a number of elements to be inserted: "))
-list =[]
-for i in range(0, n):
-    number = int(input('Enter a number: '))
-    list.append(number)
-avg = sum(list)/n
-print("avg: ", avg)   
